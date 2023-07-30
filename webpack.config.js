3742a78c25d40b3e38861e980f779d7f4c3e7e47
dim4ik85 <dim4ik020985@gmail.com>
2023-07-30 20:35:23 +0300

    add files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "index.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        }),
+        new MiniCSSExtractPlugin({
+            filename: "style.css"
+            }
+        )
+    ]
+}
